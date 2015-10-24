20e5c39bf6e69c907227035025ae114ac1ebd37d
Emily Poole <emily.poole@readify.net>
2015-10-24 14:45:54 +1000

    move origin type function to shared util

--- GenericNavigator/GenericImplementationContextSearch.cs
+++ GenericNavigator/GenericImplementationContextSearch.cs
@@ -1,13 +1,8 @@
-using System.Linq;
-using JetBrains.Application.DataContext;
-using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.CSharp;
-using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve;
-using JetBrains.ReSharper.Psi.Resolve;
-using DataConstants = JetBrains.ReSharper.Psi.Services.DataConstants;
 using JetBrains.Application.ComponentModel;
+using JetBrains.Application.DataContext;
 using JetBrains.ReSharper.Feature.Services.Navigation.ContextNavigation;
 using JetBrains.ReSharper.Feature.Services.Navigation.Requests;
+using JetBrains.ReSharper.Psi.CSharp;
 
 namespace GenericNavigator {
     [ShellFeaturePart]
@@ -21,40 +16,15 @@ namespace GenericNavigator {
             return ContextNavigationUtil.CheckDefaultApplicability<CSharpLanguage>(dataContext);
         }
 
-        protected override SearchImplementationsRequest CreateSearchRequest(IDataContext dataContext, DeclaredElementTypeUsageInfo element,
+        protected override SearchImplementationsRequest CreateSearchRequest(IDataContext dataContext,
+                                                                            DeclaredElementTypeUsageInfo element,
                                                                             DeclaredElementTypeUsageInfo initialTarget) {
-            var originTypeElement = GetOriginTypeElement(dataContext, initialTarget);
+            var originTypeElement = TypeParameterUtil.GetOriginTypeElement(dataContext, initialTarget);
             var searchDomain = SearchDomainContextUtil.GetSearchDomainContext(dataContext)
                                                       .GetDefaultDomain().SearchDomain;
             var typeParams = TypeParameterUtil.GetTypeParametersFromContext(dataContext);
 
             return new SearchGenericImplementationsRequest(element, originTypeElement, searchDomain, typeParams);
         }
-
-        private static ITypeElement GetOriginTypeElement(IDataContext dataContext, DeclaredElementTypeUsageInfo initialTarget) {
-            var data = dataContext.GetData(DataConstants.REFERENCES);
-            if (data == null) {
-                return null;
-            }
-
-            foreach (var current in data.OfType<IQualifiableReference>()) {
-                if (!Equals(current.Resolve().DeclaredElement, initialTarget)) {
-                    continue;
-                }
-
-                var qualifierWithTypeElement = current.GetQualifier() as IQualifierWithTypeElement;
-
-                if (qualifierWithTypeElement == null) {
-                    continue;
-                }
-
-                var qualifierTypeElement = qualifierWithTypeElement.GetQualifierTypeElement();
-                if (qualifierTypeElement != null) {
-                    return qualifierTypeElement;
-                }
-            }
-            return null;
-        }
-
     }
 }
--- GenericNavigator/TypeParameterUtil.cs
+++ GenericNavigator/TypeParameterUtil.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Application.DataContext;
+using JetBrains.ReSharper.Feature.Services.Navigation.ContextNavigation;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve;
 using JetBrains.ReSharper.Psi.Resolve;
 using DataConstants = JetBrains.ReSharper.Psi.Services.DataConstants;
 
@@ -25,6 +27,32 @@ namespace GenericNavigator {
             var typeParams = GetResolvedTypeParams(reference.CurrentResolveResult.Result);
 
             return typeParams;
+        }
+
+        public static ITypeElement GetOriginTypeElement(IDataContext dataContext,
+                                                        DeclaredElementTypeUsageInfo initialTarget) {
+            var data = dataContext.GetData(DataConstants.REFERENCES);
+            if (data == null) {
+                return null;
+            }
+
+            foreach (var current in data.OfType<IQualifiableReference>()) {
+                if (!Equals(current.Resolve().DeclaredElement, initialTarget)) {
+                    continue;
+                }
+
+                var qualifierWithTypeElement = current.GetQualifier() as IQualifierWithTypeElement;
+
+                if (qualifierWithTypeElement == null) {
+                    continue;
+                }
+
+                var qualifierTypeElement = qualifierWithTypeElement.GetQualifierTypeElement();
+                if (qualifierTypeElement != null) {
+                    return qualifierTypeElement;
+                }
+            }
+            return null;
         }
 
     }
